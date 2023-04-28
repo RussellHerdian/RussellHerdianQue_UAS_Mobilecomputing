ad70a39192b4c780e90f67c747a6abb5104ef695
RussellHerdianQue <blackzone644@gmail.com>
2023-04-28 16:51:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/uts_russellherdianque/RegisterActivity.kt
@@ -0,0 +1,24 @@
+package com.example.uts_russellherdianque
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        textLogin.setOnClickListener {
+            val intent = Intent ( this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        button2.setOnClickListener {
+            val intent = Intent ( this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
